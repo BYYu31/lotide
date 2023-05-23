28d0a21641f1e3c075aba4942c282c0e39805f6a
Bo Yang Yu <boyangyu93@gmail.com>
2023-05-22 23:46:45 -0400

    creat a find key value function

---
+++ findKeyByValue.js
@@ -0,0 +1,29 @@
+// FUNCTION IMPLEMENTATION
+
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`😍Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`👿Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const findKeyByValue = function(genreObject, showString) {
+  let result;
+  let genreKeys = Object.keys(genreObject);
+  for (let eachGenre of genreKeys) {
+    if (genreObject[eachGenre] === showString) {
+      result = eachGenre;
+    }
+  }
+  return result;
+}
+
+const bestTVShowsByGenre = { 
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
