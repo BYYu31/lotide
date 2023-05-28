db7508a681e123a80c237712129311a67c1bf517
Bo Yang Yu <boyangyu93@gmail.com>
2023-05-27 21:38:44 -0400

    find the key of an object with condition

---
+++ findKey.js
@@ -0,0 +1,27 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`😍Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`👿Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const findKey = function (object, callback) {
+  let result = undefined;
+  for (let i in object) {
+    if (callback(object[i])) {
+      result = i;
+      break;
+    } 
+  }
+  return result;
+}
+
+assertEqual(findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 4), undefined) // => "noma"
