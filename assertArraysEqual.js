5397895d3bb4fe4448fecad57b773d45e6fdf368
Bo Yang Yu <boyangyu93@gmail.com>
2023-06-08 20:15:37 -0400

    refactor code

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,16 +1,18 @@
-const eqArrays = function(firstArray, secondArray) {
-  let condition = true;
-  if (firstArray.length !== secondArray.length) {
-    condition = false;
-  } else {
-    for (let i = 0; i < firstArray.length; i++) {
-      if (firstArray[i] !== secondArray[i]) {
-        condition = false;
-      } 
-    }
-  }
-  return condition;
-};
+// const eqArrays = function(firstArray, secondArray) {
+//   let condition = true;
+//   if (firstArray.length !== secondArray.length) {
+//     condition = false;
+//   } else {
+//     for (let i = 0; i < firstArray.length; i++) {
+//       if (firstArray[i] !== secondArray[i]) {
+//         condition = false;
+//       } 
+//     }
+//   }
+//   return condition;
+// };
+
+const eqArrays = require('./eqArrays');
 
 const assertArraysEqual = function(array1, array2) {
   let result = eqArrays(array1,array2)
@@ -20,6 +22,8 @@ const assertArraysEqual = function(array1, array2) {
     console.log(`👿Assertion Failed: ${array1} !== ${array2}`);
   }
 };
+
+module.exports = assertArraysEqual;
 
 assertArraysEqual([1, 2, 3], [1, 2, 3]);
 assertArraysEqual([1, 2, 3], [1, 2, 4]);
--- eqArrays.js
+++ eqArrays.js
@@ -1,12 +1,14 @@
 // FUNCTION IMPLEMENTATION
 
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`😍Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`👿Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
+// const assertEqual = function(actual, expected) {
+//   if (actual === expected) {
+//     console.log(`😍Assertion Passed: ${actual} === ${expected}`);
+//   } else {
+//     console.log(`👿Assertion Failed: ${actual} !== ${expected}`);
+//   }
+// };
+
+const assertEqual = require('./assertEqual');
 
 const eqArrays = function(firstArray, secondArray) {
   let condition = true;
@@ -26,6 +28,8 @@ const eqArrays = function(firstArray, secondArray) {
   return condition;
 };
 
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
-assertEqual(eqArrays([1, 2, 3], [1, 4, 3]), false);
-assertEqual(eqArrays([1], [1, 4, "3"]), false);
+module.exports = eqArrays;
+
+// assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+// assertEqual(eqArrays([1, 2, 3], [1, 4, 3]), false);
+// assertEqual(eqArrays([1], [1, 4, "3"]), false);
