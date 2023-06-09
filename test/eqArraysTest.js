48a7aa39def8902276a1e093c4dd2dabeb8c24cf
Bo Yang Yu <boyangyu93@gmail.com>
2023-06-09 19:51:40 -0400

    refactor eqArraysTest function

--- test/eqArraysTest.js
+++ test/eqArraysTest.js
@@ -1,6 +1,7 @@
 const assertEqual = require('../assertEqual');
 const eqArrays = require('../eqArrays');
 
+// TEST CODE - Array Equal
 assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
 assertEqual(eqArrays([1, 2, 3], [1, 4, 3]), false);
 assertEqual(eqArrays([1], [1, 4, "3"]), false);
