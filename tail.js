32e35168bad0a0300cdeaa25ea94018355a576b8
Bo Yang Yu <boyangyu93@gmail.com>
2023-06-08 21:42:27 -0400

    refactor tailtest with mocha and chai

--- tail.js
+++ tail.js
@@ -9,7 +9,7 @@
 // const assertEqual = require('./assertEqual');
 
 const tail = function(array) {
-  let newArray = [];
+  let newArray;
   newArray = array.slice(1);
   return newArray;
 };
---
+++ test/tailTest.js
@@ -0,0 +1,20 @@
+// const words = ["Yo Yo", "Lighthouse", "Labs"];
+// let result = tail(words); // no need to capture the return value since we are not checking it
+// assertEqual(result.length, 2); // original array should still have 3 elements!
+// assertEqual(result[0], "Lighthouse");
+// assertEqual(result[1], "Labs");
+const tail = require('../tail');
+const { assert } = require('chai');
+
+
+// describe("#tail", () => {
+//   // it('tail of this array is ', () => {
+//   //   assert.strictEqual(tail(["Yo Yo", "Lighthouse", "Labs"]), 'Labs');
+//   // });
+// });
+
+describe('#tail', () => {
+  it('tail of this ["Yo Yo", "Lighthouse", "Labs"] is ["Lighthouse", "Labs"]', () => {
+    assert.deepEqual(tail(["Yo Yo", "Lighthouse", "Labs"]), ["Lighthouse", "Labs"]);
+  })
+});
