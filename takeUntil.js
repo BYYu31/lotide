767ba2a7ffba70dd66741ee3c04f010b2cc923e1
Bo Yang Yu <boyangyu93@gmail.com>
2023-06-09 21:50:58 -0400

    refactor takeUntil function

--- takeUntil.js
+++ takeUntil.js
@@ -1,28 +1,10 @@
-// const eqArrays = function(firstArray, secondArray) {
-//   let condition = true;
-//   if (firstArray.length !== secondArray.length) {
-//     condition = false;
-//   } else {
-//     for (let i = 0; i < firstArray.length; i++) {
-//       if (firstArray[i] !== secondArray[i]) {
-//         condition = false;
-//       } 
-//     }
-//   }
-//   return condition;
-// };
+//assertion
+const assertArraysEqual = require('./assertArraysEqual');
 
-// const assertArraysEqual = function(array1, array2) {
-//   let result = eqArrays(array1,array2)
-//   if (result) {
-//     console.log(`😍Assertion Passed: ${array1} === ${array2}`);
-//   } else {
-//     console.log(`👿Assertion Failed: ${array1} !== ${array2}`);
-//   }
-// };
+//data
+const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
 
-// const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
-
+//TAKEUNTIL FUNCTION
 const takeUntil = function(array, callback) {
   let result = [];
   for (let i of array) {
@@ -33,13 +15,14 @@ const takeUntil = function(array, callback) {
     }
   }
   return result;
-}
+};
 
+//EXPORT MODULE
 module.exports = takeUntil;
 
-// const results1 = takeUntil(data1, x => x < 0);
-// assertArraysEqual((results1), [1, 2, 5, 7, 2]);
+const results1 = takeUntil(data1, x => x < 0);
+assertArraysEqual((results1), [1, 2, 5, 7, 2]);
 
-// const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
-// const results2 = takeUntil(data2, x => x === ',');
-// assertArraysEqual((results2), ["I've", 'been', 'to', 'Hollywood']);
+const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
+const results2 = takeUntil(data2, x => x === ',');
+assertArraysEqual((results2), ["I've", 'been', 'to', 'Hollywood']);
