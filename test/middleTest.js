bfa7f61c642fd4bf329dc8011961aa0c51360b87
Bo Yang Yu <boyangyu93@gmail.com>
2023-06-09 20:45:08 -0400

    refactor middleTest function

--- test/middleTest.js
+++ test/middleTest.js
@@ -4,7 +4,7 @@ const _ = require('../index');
 // console.log(middle([1, 2, 3, 42, 22, 5, 6]));
 
 describe('#middle', () => {
-  it('the middle number of this array is', () => {
+  it('the middle number of this array [1, 2, 3, 42, 22, 5, 6] is 42', () => {
     assert.deepEqual(_.middle([1, 2, 3, 42, 22, 5, 6]), [42]);
   });
 });
