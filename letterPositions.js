e67a0e72fdec89f16cc07bf3437a85963145fab7
Bo Yang Yu <boyangyu93@gmail.com>
2023-06-09 21:16:53 -0400

    refactor lettersPositions function

--- letterPositions.js
+++ letterPositions.js
@@ -1,3 +1,7 @@
+//assertion
+const assertArraysEqual = require('./assertArraysEqual');
+
+//LETTERSPOSITIONS FUNCITON
 const letterPositions = function(sentence) {
   const results = {};
   // logic to update results here
@@ -15,56 +19,31 @@ const letterPositions = function(sentence) {
     
   }
   return results;
-}
+};
 
+//EXPORT MODULE
 module.exports = letterPositions;
 
-// const eqArrays = function(firstArray, secondArray) {
-//   let condition = true;
-//   if (firstArray === undefined || secondArray === undefined) {
-//     condition = false;
-//     return condition;
-//   }
-//   if (firstArray.length !== secondArray.length) {
-//     condition = false;
-//   } else {
-//     for (let i = 0; i < firstArray.length; i++) {
-//       if (firstArray[i] !== secondArray[i]) {
-//         condition = false;
-//       } 
-//     }
-//   }
-//   return condition;
-// };
+//TEST
+assertArraysEqual(letterPositions('hello').l, [2,3]);
 
-// const assertArraysEqual = function(array1, array2) {
-//   let result = eqArrays(array1,array2)
-//   if (result) {
-//     console.log(`😍Assertion Passed: ${array1} === ${array2}`);
-//   } else {
-//     console.log(`👿Assertion Failed: ${array1} !== ${array2}`);
-//   }
-// };
+const test1 = letterPositions('hello');
+// h: [0], e: [1], l: [2, 3], o: [4]
+assertArraysEqual(test1['h'], [0]);
+assertArraysEqual(test1['e'], [1]);
+assertArraysEqual(test1['l'], [2, 3]);
+assertArraysEqual(test1['o'], [4]);
 
-// assertArraysEqual(letterPositions('hello').l, [2,3]);
-
-// const test1 = letterPositions('hello');
-// // h: [0], e: [1], l: [2, 3], o: [4]
-// assertArraysEqual(test1['h'], [0]);
-// assertArraysEqual(test1['e'], [1]);
-// assertArraysEqual(test1['l'], [2, 3]);
-// assertArraysEqual(test1['o'], [4]);
-
-// const test2 = letterPositions('web developement');
-// // w: [0], e: [1, 5, 7, 11, 13], b: [2], d: [4], v: [6], l: [8], o: [9], p: [10], m: [12], n: [14], t: [15]
-// assertArraysEqual(test2['w'], [0]);
-// assertArraysEqual(test2['e'], [1, 5, 7, 11, 13]);
-// assertArraysEqual(test2['b'], [2]);
-// assertArraysEqual(test2['d'], [4]);
-// assertArraysEqual(test2['v'], [6]);
-// assertArraysEqual(test2['l'], [8]);
-// assertArraysEqual(test2['o'], [9]);
-// assertArraysEqual(test2['p'], [10]);
-// assertArraysEqual(test2['m'], [12]);
-// assertArraysEqual(test2['n'], [14]);
-// assertArraysEqual(test2['t'], [15]);
+const test2 = letterPositions('web developement');
+// w: [0], e: [1, 5, 7, 11, 13], b: [2], d: [4], v: [6], l: [8], o: [9], p: [10], m: [12], n: [14], t: [15]
+assertArraysEqual(test2['w'], [0]);
+assertArraysEqual(test2['e'], [1, 5, 7, 11, 13]);
+assertArraysEqual(test2['b'], [2]);
+assertArraysEqual(test2['d'], [4]);
+assertArraysEqual(test2['v'], [6]);
+assertArraysEqual(test2['l'], [8]);
+assertArraysEqual(test2['o'], [9]);
+assertArraysEqual(test2['p'], [10]);
+assertArraysEqual(test2['m'], [12]);
+assertArraysEqual(test2['n'], [14]);
+assertArraysEqual(test2['t'], [15]);
