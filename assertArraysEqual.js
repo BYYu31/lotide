097d9aff93530e24233516cd82353ad168975e30
Bo Yang Yu <boyangyu93@gmail.com>
2023-06-09 20:14:14 -0400

    refactor asserArraysEqual function

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,21 +1,7 @@
-// const eqArrays = function(firstArray, secondArray) {
-//   let condition = true;
-//   if (firstArray.length !== secondArray.length) {
-//     condition = false;
-//   } else {
-//     for (let i = 0; i < firstArray.length; i++) {
-//       if (firstArray[i] !== secondArray[i]) {
-//         condition = false;
-//       } 
-//     }
-//   }
-//   return condition;
-// };
-
 const eqArrays = require('./eqArrays');
 
 const assertArraysEqual = function(array1, array2) {
-  let result = eqArrays(array1,array2)
+  let result = eqArrays(array1,array2);
   if (result) {
     console.log(`😍Assertion Passed: ${array1} === ${array2}`);
   } else {
@@ -25,8 +11,3 @@ const assertArraysEqual = function(array1, array2) {
 
 module.exports = assertArraysEqual;
 
-// assertArraysEqual([1, 2, 3], [1, 2, 3]);
-// assertArraysEqual([1, 2, 3], [1, 2, 4]);
-// assertArraysEqual([1, 3, 3], [1, 2, 3]);
-// assertArraysEqual([1, 2, 'lki'], [1, 2, 3]);
-// assertArraysEqual([1], [1, 2, 3]);
