6e6121a5781f0dca5a977e919cc3954ae38273ad
Bo Yang Yu <boyangyu93@gmail.com>
2023-06-09 20:11:08 -0400

    refactor assertEqual function

--- assertEqual.js
+++ assertEqual.js
@@ -10,9 +10,3 @@ const assertEqual = function(actual, expected) {
 
 module.exports = assertEqual;
 
-// TEST CODE
-// assertEqual("Lighthouse Labs", "Bootcamp");
-// assertEqual(1, 1);
-// assertEqual('my name is', 'my name is');
-// assertEqual('what is my name?', 'what is my name');
-// assertEqual(44, 2980);
